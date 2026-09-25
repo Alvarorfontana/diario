dfbae83f3b72a6d128697d3fbb7e86c176c35780
Alvarorfontana <alvarorfontana@gmail.com>
2026-09-25 11:09:07 -0300

    Update [slug].png.ts

--- src/pages/   og/[slug].png.ts
+++ src/pages/   og/[slug].png.ts
@@ -131,4 +131,38 @@ export async function GET({ props, site }: { props: any; site: URL }) {
 				children: {
 					type: 'img',
 					props: {
-						src: imageUrl
+						src: imageUrl,
+						width: 1200,
+						height: 390,
+						style: { width: '1200px', height: '390px', objectFit: 'cover' },
+					},
+				},
+			},
+		});
+	}
+	children.push(panelTexto);
+
+	return new ImageResponse(
+		{
+			type: 'div',
+			props: {
+				style: {
+					width: '1200px',
+					height: '630px',
+					display: 'flex',
+					flexDirection: 'column',
+					background: '#f6efdf',
+				},
+				children,
+			},
+		},
+		{
+			width: 1200,
+			height: 630,
+			fonts: [
+				{ name: 'Lora', data: loraBold, weight: 700, style: 'normal' },
+				{ name: 'Inter', data: interBold, weight: 700, style: 'normal' },
+			],
+		}
+	);
+}
